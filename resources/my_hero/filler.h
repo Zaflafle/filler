06280524a1c07c71802866e4a8e527d165c9a188
Macuguen <macuguen@student.42.fr>
2018-04-26 20:55:11 +0200

    trouver le chemin pour x

--- resources/my_hero/filler.h
+++ resources/my_hero/filler.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   filler.h                                           :+:      :+:    :+:   */
+/*   filler.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: macuguen <macuguen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/22 15:15:31 by macuguen          #+#    #+#             */
-/*   Updated: 2018/04/22 21:26:25 by macuguen         ###   ########.fr       */
+/*   Updated: 2018/04/23 21:16:07 by macuguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,21 @@ typedef struct	s_filler
     int i;
     int j;
     int fd;
+
     int length_map_x;
     int length_map_y;
+    int length_parts_x;
+    int length_parts_y;
+
+    int distance;
+    int player_x;
+    int player_y;
+    int enemy_x;
+    int enemy_y;
+
+    int try_parts_x;
+    int try_parts_y;
+    int star;
 }				t_filler;
 
 typedef struct	s_map
@@ -29,6 +42,12 @@ typedef struct	s_map
     char map_zero[15][17];
     char map_one[24][40];
     char map_two[100][99];
+
+    char tmp_zero[15][17];
+    char tmp_one[24][40];
+    char tmp_two[100][99];
+
+    char parts[100][99];
 }	            t_map;
  
 #endif      
--- resources/my_hero/test.c
+++ resources/my_hero/test.c
@@ -6,12 +6,159 @@
 /*   By: macuguen <macuguen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 18:59:37 by macuguen          #+#    #+#             */
-/*   Updated: 2018/04/22 23:30:28 by macuguen         ###   ########.fr       */
+/*   Updated: 2018/04/26 20:52:22 by macuguen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "filler.h"
+
+// void    parse_parts(t_filler *env, t_map *map)
+// {
+//     while (x <= env->length_parts_x)
+
+// }
+void    go_back(t_filler *env, t_map *map)
+{
+    int x;
+    int y;
+
+    x = env->player_x;
+    while (x >= 0)
+    {
+        y = env->player_y;
+        while (y >= 0)
+        {
+            if (map->map_one[x][y] == 'O')
+            {
+                env->player_x = x;
+                env->player_y = y;
+            }
+            y--;
+        }
+        x--;
+    }
+}
+
+void    solveur(t_filler *env, t_map *map)
+{
+    int i;
+
+    i = 0;
+    while (i <= env->star)
+    {
+        if (i == 0 && map->map_one[env->player_x][env->player_y] == 'O')
+            check_is_valid(env, map);
+        else if (map->map_one[env->player_x + env->try_parts_x][env->player_y + env->try_parts_y] == '.')
+            check_is_valid(env, map);
+        else
+        {
+            i = 0;
+            go_back(env, map);
+        }
+    }
+        
+}
+
+void    count_star(t_filler *env, t_map *map)
+{
+    int x;
+    int y;
+
+    x = 0;
+    y = 0;
+    while (x <= env->length_parts_x)
+    {
+        y = 0;
+        while (y <= env->length_parts_y)
+        {
+            if (map->parts[x][y] == '*')
+                env->star++;
+            y++;            
+    }
+}
+
+void    check_is_valid(t_filler *env, t_map *map)
+{
+    int x;
+    int y;
+
+    x = env->try_parts_x;
+    x = env->try_parts_y;
+    while (x <= env->length_parts_x)
+    {
+        y = 0;
+        while (map->parts[x][y] != '*')
+            y++;
+        if (map->parts[x][y] != '*')
+        {
+            env->try_parts_x = x;
+            env->try_parts_y = y;
+            break;
+        }
+    }
+}
+
+void    parse_map_one(t_filler *env, t_map *map)
+{
+    int x;
+    int y;
+
+    while (x <= 24)
+    {
+        y = 0;
+        while (y <= 40)
+        {
+            if(map->map_one[x][y] == 'O')
+            {
+                env->player_x = x;
+                env->player_y = y;
+                env->distance = 0;
+            }
+            else if (map->map_one[x][y] == 'X')
+            {
+                env->enemy_x = x;
+                env->enemy_y = y;                
+                break;
+            }
+            env->distance++;
+            y++;
+        }
+        x++;
+    }
+    dprintf(env->fd, "player_x = {%d} player_y = {%d} enemy_x = {%d} enemy_y = {%d} distance = {%d} \n",
+    env->player_x, env->player_y, env->enemy_x, env->enemy_y, env->distance);
+}
+
+void    ft_bzero_double(t_map *map, int n, int m)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while (i <= n)
+    {
+        ft_bzero(map->parts[i], (size_t)m);
+        i++;
+    }
+}
+
+void    init_parts_and_filling(char *str, t_filler *env, t_map *map)
+{
+    int i;
+    int k;
+
+    i = 0;
+    k = 0;
+    while (i <= env->length_parts_y)
+    {
+        map->parts[env->j][k] = str[i];
+        i++;
+        k++;
+    }
+   // dprintf(env->fd, "%d|%s|\n", env->j, map->parts[env->j]);    
+}
 
 void    filling_map_zero(char *str, t_filler *env, t_map *map)
 {
@@ -26,7 +173,7 @@ void    filling_map_zero(char *str, t_filler *env, t_map *map)
         i++;
         k++;
     }
-    dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
+   // dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
 }
 void    filling_map_one(char *str, t_filler *env,  t_map *map)
 {
@@ -41,7 +188,7 @@ void    filling_map_one(char *str, t_filler *env,  t_map *map)
         i++;
         k++;
     }
-    dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
+    //dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
 }
 void    filling_map_two(char *str, t_filler *env,  t_map *map)
 {
@@ -56,20 +203,34 @@ void    filling_map_two(char *str, t_filler *env,  t_map *map)
         i++;
         k++;
     }
-    dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
+    //dprintf(env->fd, "%d|%s|\n", env->j, map->map_one[env->j]);
 }
 
 void    init_map_and_filling(char *str, t_filler *env, t_map *map)
 {
     if (env->length_map_x == 15 && env->length_map_y == 17)
         filling_map_zero(str, env, map);
-    // else if (env->length_map_x == 24 && env->length_map_y == 40)
-    //     filling_map_one(str, env, map);
-    // else if (env->length_map_x == 100 && env->length_map_y == 99)
-    //     filling_map_two(str, env, map);
+    else if (env->length_map_x == 24 && env->length_map_y == 40)
+        filling_map_one(str, env, map);
+    else if (env->length_map_x == 100 && env->length_map_y == 99)
+        filling_map_two(str, env, map);
 
 }
 
+void length_of_the_parts(char *str, t_filler *env)
+{
+    int i;
+
+    i = 0;
+    while (ft_isdigit(str[i]) != 1)
+        i++;
+    env->length_parts_x = ft_atoi(&str[i]);
+    i = i + 2;
+    while (ft_isdigit(str[i]) != 1)
+        i++;
+    env->length_parts_y = ft_atoi(&str[i]);
+    dprintf(env->fd, "length_parts_x = {%d} length_parts_y = {%d}\n", env->length_parts_x, env->length_parts_y);
+}
 
 void length_of_the_map(char *str, t_filler *env)
 {
@@ -128,6 +289,22 @@ int main()
                 get_next_line(0, &str);                
                 env.j++;
             }
+            if (ft_strncmp("Piece", str, 5) == 0)
+            {
+                length_of_the_parts(str, &env);
+                env.j = 0; 
+                ft_bzero_double(&map, env.length_parts_x, env.length_parts_y);
+                    get_next_line(0, &str);                
+                while (env.j <= env.length_parts_x)
+                {
+                    dprintf(env.fd, "%s\n", str);
+                    init_parts_and_filling(str, &env, &map);
+                    get_next_line(0, &str);
+                    env.j++;
+                }
+            }
+            parse_map_one(&env, &map);
+            // printf("4 4\n");
         }
         env.i++;
         dprintf(env.fd, "%s\n", str);
